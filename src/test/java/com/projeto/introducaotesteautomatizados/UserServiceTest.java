1c8fae3c62876f34c5b3b13f20ce25a921607df6
Leonardo Nascimento <omgleo@live.com>
2023-02-21 23:36:51 -0300

    2 cenario de teste com erro

--- src/test/java/com/projeto/introducaotesteautomatizados/UserServiceTest.java
+++ src/test/java/com/projeto/introducaotesteautomatizados/UserServiceTest.java
@@ -1,7 +1,7 @@
 package com.projeto.introducaotesteautomatizados;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 
@@ -37,6 +37,22 @@ public class UserServiceTest {
    assertEquals(user, users.get(0));
 }
 
+@Test
+//cenario de error
+//se tentar criar um usuario  que ja tem o username sendo utilizado eu vou querer que seja criado um Exception
+public void createUser_ThrowsException(){
 
+
+   
+   User user =  new User("Leoanardo", "1234556");
+   userService.create(user);
+   
+   
+   //quando executar um create , com os dados  de usuario que  ja existe quero que seja lançado um Exception
+   assertThrows(RuntimeException.class,
+   ()->  userService.create(user));
+   
+
+}
 
 }
